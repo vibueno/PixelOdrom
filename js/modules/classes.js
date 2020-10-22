862f9afcb6523dfa80164f30520b5791d86ea571
vibueno <victor@localhost>
2020-10-22 12:14:12 +0200

    Created basic Class structure

--- js/modules/classes.js
+++ js/modules/classes.js
@@ -3,4 +3,50 @@
  */
 
 import { functions } from './functions.js';
-import { } from './constants.js';
+import { PIXEL_CANVAS_SEL } from './constants.js';
+
+/**
+ * @constructor
+ * @description Creates a new Canvas object.
+ *
+ */
+let Pixel = function(){
+
+}
+
+/**
+ * @constructor
+ * @description Creates a new Canvas object.
+ *
+ * @property {Object}  DOMNode DOM object related to the canvas.
+ */
+let Canvas = function(){
+	DOMNode = $ ( PIXEL_CANVAS_SEL );
+}
+
+/**
+ * @constructor
+ * @description Creates a new DrawingTool object.
+ *
+ */
+let DrawingTool = function(){
+
+}
+
+/**
+ * @constructor
+ * @description Creates a new Spin object.
+ *
+ */
+let Spin = function(){
+
+}
+
+/**
+ * @constructor
+ * @description Creates a new Dialog object.
+ *
+ */
+let Dialog = function(){
+
+}
